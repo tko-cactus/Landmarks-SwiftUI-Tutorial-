6a7db77e1134449b268b81e5dd851c7865d41cf0
tko-cactus <37374201+tko-cactus@users.noreply.github.com>
2021-01-05 22:55:12 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yamaguchi Tokio on 2021/01/05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
